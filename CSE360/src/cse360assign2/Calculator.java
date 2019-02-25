a86d6a60604adf9304f9bdb37c1e2b9952739da2
prest <prest@192.168.0.47>
2019-02-25 10:18:18 -0700

    Version 1.0 of the Calculator java file.

---
+++ CSE360/src/cse360assign2/Calculator.java
@@ -0,0 +1,82 @@
+/**
+* @author Charles Allgaier
+* @classID: 313
+* @assignment: #2
+* @version: 1.0
+* @description: This program performs the various functions of a calculator. (For example: add, subtract, divide, etc.)
+*/
+
+package cse360assign2;
+
+public class Calculator {
+	
+	/**
+	* Adds, subtracts, multiplies and divides integer values. 
+	* 
+	* @param total   The total integer value after the arithmetic 
+	*                operations are performed.
+	*/
+	private int total;
+	
+	/**
+	* Constructs a Calculator class when called.
+	*/
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	* Displays the value of the total parameter.
+	*
+	* @return   The total parameter.
+	*/
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	* Adds the integer value to the total parameter.
+	* 
+	* @param value   The value to be added.
+	*/
+	public void add (int value) {
+		
+	}
+	
+	/**
+	* Subtracts the integer value from the total parameter. 
+	* 
+	* @param value   The value to be subtracted.
+	*/
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	* Multiplies the total parameter by the integer value.
+	* 
+	* @param value   The value to be multiplied by.
+	*/
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	* Divides the total parameter by the integer value. 
+	* 
+	* @param value   The value to be divided by.
+	*/
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	* Returns a string containing all operations performed on the
+	* total parameter. 
+	* 
+	* @return   String containing all operations performed.
+	*/
+	public String getHistory () {
+		return "";
+	}
+}
